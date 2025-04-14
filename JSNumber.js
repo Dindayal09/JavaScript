3d43d3ddc4a4425b80bdb1f2c97ee52fbce24c70
Dindayal09 <dindayalgupta12354@gmail.com>
2025-04-14 15:16:17 +0000

    container config done

---
+++ JSNumber.js
@@ -0,0 +1,15 @@
+//JavaScript has only one type of number. Numbers can be written with or without decimals.
+let num = 10;
+let num1 = 10.25;
+
+//Extra large or extra small numbers can be written with scientific (exponent) notation
+
+//Extra large number
+let num2 = 123e5;
+
+//Extra small number
+let num3 = 123e-7;
+
+console.table([num,num1,num2,num3]);
+
+
