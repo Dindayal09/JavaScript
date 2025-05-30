f2a12921e3decebee07eea81cce07ff8fe110b9f
Dindayal09 <dindayalgupta12354@gmail.com>
2025-05-30 19:32:49 +0000

    variables files

---
+++ 01_Practice/Variables.js
@@ -0,0 +1,25 @@
+// Variables are Containers for Storing Data
+// JavaScript Variables can be declared in 4 ways:
+
+// Automatically
+// Using var
+// Using let
+// Using const
+
+// var have a global scope even var is defined in any scope or function.
+
+// let and const have block scope
+
+// The let keyword was introduced in ES6 (2015)
+// Variables declared with let have Block Scope
+// Variables declared with let must be Declared before use
+// Variables declared with let cannot be Redeclared in the same scope
+
+// The const keyword was introduced in ES6 (2015)
+// Variables defined with const cannot be Redeclared
+// Variables defined with const cannot be Reassigned
+// Variables defined with const have Block Scope
+
+var x = 5;
+
+console.log(x);
