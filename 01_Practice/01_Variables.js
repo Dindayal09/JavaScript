ec6bb294ab3fec8f172ae16d8707064013c4ead7
Dindayal09 <dindayalgupta12354@gmail.com>
2025-05-30 20:19:54 +0000

    update

---
+++ 01_Practice/01_Variables.js
@@ -0,0 +1,55 @@
+// Variables are Containers for Storing Data
+// JavaScript Variables can be declared in 4 ways:
+
+// Automatically
+// Using var
+// Using let
+// Using const
+
+// var have a global scope even var is defined in any scope or function.
+
+// let and const have block scope
+
+// The let keyword was introduced in ES6 (2015)
+// Variables declared with let have Block Scope
+// Variables declared with let must be Declared before use
+// Variables declared with let cannot be Redeclared in the same scope
+
+// The const keyword was introduced in ES6 (2015)
+// Variables defined with const cannot be Redeclared
+// Variables defined with const cannot be Reassigned
+// Variables defined with const have Block Scope
+
+var x = 5;
+console.log("Global scope: ",x);
+
+//lets redefine x value and function and try what result will get
+
+var x = 10;
+
+console.log("Now x value is override: ",x);
+
+{
+    var x = 15;
+}
+
+console.log("It access from out side: ",x);
+
+//let - once declared then now allowed to redeclared in same scope but reassign is allowed
+//example 
+let y = 10;
+console.log("Declared with let: ",y);
+
+// let y = 15; //it is show the error
+
+//but once declared with let it allowed to reassign.
+
+y = 20;
+
+console.log("Reassigning the value of y:  ",y);
+
+//Always declare a variable with const when you know that the value should not be changed.
+
+
+
+
---
+++ 01_Practice/02_Operator.js
@@ -0,0 +1,24 @@
+// Javascript operators are used to perform different types of mathematical and logical computations.
+// Examples:
+// The Assignment Operator = assigns values
+// The Addition Operator + adds values
+// The Multiplication Operator * multiplies values
+// The Comparison Operator > compares values
+
+let number1 = 5;  // 5 is assign to 'number1'
+let number2 = 10;
+
+let results = number1 + number2;
+console.log(`Results of ${number1} + ${number2} =  ${results}`);
+
+results = number1 * number2; 
+console.log(`Results of ${number1} * ${number2} =  ${results}`);
+
+results = number1 / number2;
+console.log(`Results of ${number1} / ${number2} =  ${results}`);
+
+results = number1 % number2;
+console.log(`Results of ${number1} % ${number2} =  ${results}`);
+
+results = number1 - number2;
+console.log(`Results of ${number1} - ${number2} =  ${results}`);
